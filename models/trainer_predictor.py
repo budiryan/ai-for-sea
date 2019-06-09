bae5f3dbcadc69265cd56fe650b552c797db80fd
budiryan <budiryan@gmail.com>
2019-06-09 21:02:16 +0700

    save models

--- models/trainer_predictor.py
+++ models/trainer_predictor.py
@@ -24,19 +24,20 @@ def train_cv_predict_test(clf, num_splits, X, y, X_test):
     return oof_train.reshape(-1, 1), oof_test.reshape(-1, 1)
 
 
-def train_cv(clf, num_splits, X, y):
+def train_cv(clf, clf_params, num_splits, X, y):
     oof_train = np.zeros((len(X),))
 
-    for i, (train_index, test_index) in enumerate(StratifiedKFold(n_splits=num_splits, shuffle=True).split(X, y)):
+    for i, (train_index, val_index) in enumerate(StratifiedKFold(n_splits=num_splits, shuffle=True).split(X, y)):
         print('Training for fold: ', i + 1)
 
+        c = clf(clf_params)
         x_tr = X[train_index, :]
         y_tr = y[train_index]
-        x_te = X[test_index, :]
-        y_te = y[test_index]
+        x_val = X[val_index, :]
+        y_val = y[val_index]
 
-        clf.train(x_tr, y_tr, x_val=x_te, y_val=y_te)
+        c.train(x_tr, y_tr, x_val=x_val, y_val=y_val)
 
-        oof_train[test_index] = clf.predict(x_te)
+        oof_train[val_index] = c.predict(x_val)
 
     return oof_train.reshape(-1, 1)
